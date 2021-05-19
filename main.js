7979c718a7cd6970ab2ff5297bc2c6007fcc5f1a
Alex-Demha <79983650+Alex-Demha@users.noreply.github.com>
2021-05-19 08:50:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// making a timed function that scrolls through the sliders every 6 seconds automatically (auto scroll)
 var interval;
 var timer;
 timer = function()
@@ -9,19 +10,28 @@ timer();
 
 $(".arrow").on('click', function(){
     
+    //checking if its the right(next) or left(prev) arrow
     if($(this).hasClass('next'))
     {
+        //when an arrow is cliked the timer for auto scroll is reset.
         clearInterval(interval);
         timer();
+
+        //making variables for the currently active slide and its marker
         const $El = $(".active");
         const $activeMark = $(".markActive");
+
+        //checking if the user on the last slide, pressing next-arrow would take him back to the first slide
         if ($El.hasClass("text3")) {
             $('.text1').addClass("active").addClass('showNext').removeClass("showPrev");
             $('.mark1').addClass("markActive");
             $El.removeClass("active").removeClass("showNext").removeClass("showPrev");
             $('.mark3').removeClass('markActive');
         }
+
+        //taking the user to the next slide
         else{
+            //adding and removing classes to the apropriate slides and markers.
             $('.active').next().addClass('active').addClass('showNext').removeClass('showPrev');
             $('.markActive').next().addClass('markActive')
             $El.removeClass('active').removeClass("showNext").removeClass("showNext");
@@ -29,6 +39,8 @@ $(".arrow").on('click', function(){
         }
     }
 
+
+    //same as above but in reverse. next is prev
     if($(this).hasClass('prev'))
     {
         clearInterval(interval);
